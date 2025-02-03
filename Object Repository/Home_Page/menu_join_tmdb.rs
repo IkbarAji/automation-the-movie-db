4a509160447d40b20e37d5e88e5028795020c2b2
Ikbar Aji Hawali <ikbarajih@gmail.com>
2025-02-03 20:23:01 +0700

    add new test cases

---
+++ Object Repository/Home_Page/menu_join_tmdb.rs
@@ -0,0 +1,138 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>menu_join_tmdb</name>
+   <tag></tag>
+   <elementGuidId>0453067b-db56-45af-baa3-35df3cb3c5d8</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//a[contains(text(),'Join TMDB')]</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>div.flex > ul.primary > li:nth-of-type(4) > a</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorCollection>
+      <entry>
+         <key>SMART_LOCATOR</key>
+         <value>internal:label=&quot;Join TMDB&quot;i</value>
+      </entry>
+   </smartLocatorCollection>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>a</value>
+      <webElementGuid>601bf910-f27d-4dcb-a838-4737d5c5cb89</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>href</name>
+      <type>Main</type>
+      <value>/signup</value>
+      <webElementGuid>6a98a4ab-dcb8-463c-b153-56c988360c11</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>aria-label</name>
+      <type>Main</type>
+      <value>Join TMDB</value>
+      <webElementGuid>6638d733-90fa-4e31-822d-2e0bf31b71c2</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Join TMDB</value>
+      <webElementGuid>73503820-6e39-4bad-bd79-9defa8abe16d</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[@class=&quot;no-js k-webkit k-webkit127&quot;]/body[@class=&quot;en v4&quot;]/div[@class=&quot;page_wrap _wrap&quot;]/header[@class=&quot;normal smaller no_animation smaller  subtle&quot;]/div[@class=&quot;content&quot;]/div[@class=&quot;sub_media&quot;]/div[@class=&quot;flex&quot;]/ul[@class=&quot;primary&quot;]/li[4]/a[1]</value>
+      <webElementGuid>d6c35022-23bf-447b-9374-f6c5b3aaef42</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:link</name>
+      <type>Main</type>
+      <value>//a[contains(text(),'Join TMDB')]</value>
+      <webElementGuid>5d19f704-ebe1-4ab4-bf7a-ee58b1e3cc58</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Login'])[1]/following::a[1]</value>
+      <webElementGuid>c83e8cf0-2851-4985-b916-988d44295891</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='en'])[1]/following::a[2]</value>
+      <webElementGuid>4a6ec775-4b57-4606-a3a2-b307be03e15d</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Welcome.'])[1]/preceding::a[3]</value>
+      <webElementGuid>ef0ceef1-df17-49b0-9564-0067552bf4d8</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Millions of movies, TV shows and people to discover. Explore now.'])[1]/preceding::a[3]</value>
+      <webElementGuid>650f615f-debe-402f-8b25-75326c38904a</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='Join TMDB']/parent::*</value>
+      <webElementGuid>eb8a8c7e-c996-4db7-ac45-1336361651c7</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:href</name>
+      <type>Main</type>
+      <value>//a[contains(@href, '/signup')]</value>
+      <webElementGuid>0ffd0b91-357d-4fcc-beb8-1681b2ac3209</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[2]/ul/li[4]/a</value>
+      <webElementGuid>d94c2de2-95fa-49b4-968d-17663566edc0</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//a[@href = '/signup' and (text() = 'Join TMDB' or . = 'Join TMDB')]</value>
+      <webElementGuid>5f9736b8-2001-411c-9211-aa1c8c4a1d38</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
